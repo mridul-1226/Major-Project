2f8fccfe55131acdc66614982d7106b7b7e4263e
mridul-1226 <mmridul1826@gmail.com>
2024-10-11 12:37:16 +0530

    registration

---
+++ major_project/lib/auth_feature/presentation/regitartion_page.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+
+  static Route getRoute(){
+    return MaterialPageRoute(builder: (context) => const RegistrationPage(),);
+  }
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final PageController _pageController = PageController();
+  final List<GlobalKey<FormState>> _formKeys = [
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+  ];
+  final TextEditingController _dateController = TextEditingController();
+  int _currentPage = 0;
+  String? _selectedBranch;
+  String? _selectedGender;
+
+  final List<String> _branches = ['CSE', 'EE', 'EL', 'Mining'];
+  final List<String> _genders = ['Male', 'Female', 'Other'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.blue.shade300, Colors.purple.shade300],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Registration',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  onPageChanged: (int page) {
+                    setState(() {
+                      _currentPage = page;
+                    });
+                  },
+                  children: [
+                    _buildPage(0),
+                    _buildPage(1),
+                    _buildPage(2),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (_currentPage > 0)
+                      ElevatedButton(
+                        onPressed: () {
+                          _pageController.previousPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white, foregroundColor: Colors.blue),
+                        child: const Text('Previous'),
+                      ),
+                    if (_currentPage < 2)
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKeys[_currentPage].currentState!.validate()) {
+                            _pageController.nextPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white, foregroundColor: Colors.blue),
+                        child: const Text('Next'),
+                      ),
+                    if (_currentPage == 2)
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKeys.every((key) => key.currentState!.validate())) {
+                            // TODO: Implement registration logic
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Registration')),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white, foregroundColor: Colors.blue),
+                        child: const Text('Register'),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage(int pageIndex) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKeys[pageIndex],
+          child: Column(
+            children: [
+              if (pageIndex == 0) ...[
+                _buildInputField('Name'),
+                _buildInputField('Mobile Number', TextInputType.phone),
+                _buildDropdownField('Gender', _genders, (value) => _selectedGender = value),
+              ],
+              if (pageIndex == 1) ...[
+                _buildInputField("Parent's Name"),
+                _buildInputField("Parent's Mobile Number", TextInputType.phone),
+                _buildDateField(),
+              ],
+              if (pageIndex == 2) ...[
+                _buildInputField('Year of Joining College', TextInputType.number),
+                _buildDropdownField('Branch', _branches, (value) => _selectedBranch = value),
+                _buildInputField('Address', TextInputType.multiline, 3),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField(String label, [TextInputType? keyboardType, int maxLines = 1]) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: label,
+              border: InputBorder.none,
+            ),
+            keyboardType: keyboardType,
+            maxLines: maxLines,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter $label';
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDateField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: TextFormField(
+            controller: _dateController,
+            decoration: const InputDecoration(
+              labelText: 'Date of Birth',
+              border: InputBorder.none,
+            ),
+            readOnly: true,
+            onTap: () async {
+              DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime.now(),
+              );
+              if (pickedDate != null) {
+                String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+                setState(() {
+                  _dateController.text = formattedDate;
+                });
+              }
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your date of birth';
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDropdownField(String label, List<String> items, Function(String?) onChanged) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              labelText: label,
+              border: InputBorder.none,
+            ),
+            items: items.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: onChanged,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please select a $label';
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
